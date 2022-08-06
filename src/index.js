65147d4941b11827a132f310d8262e53e12e7d2d
humanshield85 <rachidboudjelida@gmail.com>
2022-08-06 22:47:40 +0100

    export a way to setup cloud code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const { Uint8ArrayFromHexString, HexStringFromUint8Array } = require("./lib/Utils");
 
 const { NearAuthAdapter } = require("./lib/NearAuthAdapter");
+const { getNearSignToken } = require("./lib/CloudCode/NearAuthCloudCode");
 
 exports.Uint8ArrayFromHexString = Uint8ArrayFromHexString;
 
 exports.HexStringFromUint8Array = HexStringFromUint8Array;
 
 exports.NearAuthAdapter = NearAuthAdapter;
+
+exports.setupNearAuthAdapterCloudCode = () => {
+  Parse.Cloud.define("getNearSignToken", getNearSignToken);
+};
